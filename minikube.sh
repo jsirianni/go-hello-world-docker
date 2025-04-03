e8e11986838566abb49780691d0f63e3b153dd93
Joe Sirianni <joe.sirianni@observiq.com>
2025-04-03 15:53:34 -0400

    hpa

--- minikube.sh
+++ minikube.sh
@@ -4,10 +4,14 @@ set -e
 
 minikube delete
 minikube start
-minikube addons enable ingress || minikube addons enable ingress
+minikube addons enable ingress
 
 docker tag go-hello-world:latest bmedora/go-hello-world:latest
 docker push bmedora/go-hello-world:latest
+
+# wait for ingress-nginx to be ready
+kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller
+sleep 5
 
 kubectl apply -f deployment.yaml
 
