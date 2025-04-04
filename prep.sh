5aa85835a7637add7a990653069a4f724dc83356
Joe Sirianni <joe.sirianni@observiq.com>
2025-04-04 09:38:12 -0400

    ensure image

--- prep.sh
+++ prep.sh
@@ -4,6 +4,7 @@ minikube delete
 minikube start
 minikube addons enable ingress
 
+docker build -t bmedora/go-hello-world:latest .
 docker push bmedora/go-hello-world:latest
 
 # wait for ingress-nginx to be ready
